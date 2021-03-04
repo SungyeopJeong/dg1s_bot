b643cc529c2e9fb9d6206fb98faeb98a166b4c31
정성엽 <71690205+SungyeopJeong@users.noreply.github.com>
2021-03-05 08:02:13 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -190,7 +190,7 @@ def what_is_menu(): # made by 1316, 1301
               menu_text = menu.get_text(); print(menu_text)
               bracket_i = menu_text.find('[')
               bracket_j = menu_text.find(']')
-              menu_day = menu_text[:bracket_i]; print(menu_day)
+              menu_day = menu_text[:bracket_i]; print('"'+menu_day+'"')
               menu_when = menu_text[bracket_i+1:bracket_j]
               menu_content = menu_text[bracket_j+3:].rstrip().replace(" ","\n")
               
