5f4b112c5a691e0e3d250c3aa3008d7d8bd23bac
정성엽 <71690205+SungyeopJeong@users.noreply.github.com>
2021-03-26 23:41:49 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -429,14 +429,12 @@ def input_seat(): # 좌석 번호 입력 함수
                                 }
                             }
                         ],
-                        {
-                            "quickReplies": {
-                                "type": "block",
-                                "label": "test",
-                                "message": "테스트",
-                                "data": {
-                                  
-                                }
+                        "quickReplies": {
+                            "type": "block",
+                            "label": "test",
+                            "message": "테스트",
+                            "data": {
+                              
                             }
                         }
                     }
